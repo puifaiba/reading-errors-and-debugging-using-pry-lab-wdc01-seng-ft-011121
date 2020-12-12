6c6c317f3da0f4efaa67ffbbe3f55193c60e1daa
Boonie Attawut <puifai.ba@gmail.com>
2020-12-12 06:40:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10.times.to_s do "s" end + string
+  if string[0] == s
+    10.times do s end + string
   else
     string
   end
